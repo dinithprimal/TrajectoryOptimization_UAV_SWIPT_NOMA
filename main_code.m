a53671e2cda65d82d7230cc549ec8c6917682d9c
dinithprimal <dinithprimal@gmail.com>
2023-07-28 20:27:50 +0530

    28/07/2023 20:27

--- main_code.m
+++ main_code.m
@@ -107,7 +107,329 @@ end
 
 % Plotting UAV default path
 for i = 1:noUAV
-    plot3([xUAV_S(i) xUAV_E(i)], [yUAV_S(i) yUAV_E(i)],[zUAV_S(i) zUAV_E(i)], 'y--','linewidth', 0.5); drawnow
+    plot3([xUAV_S(i) xUAV_E(i)], [yUAV_S(i) yUAV_E(i)],[zUAV_S(i) zUAV_E(i)], '--', 'Color', '#660000' ,'linewidth', 1); drawnow
 end
+
+mx = (zUAV_S - zUAV_E)/(yUAV_S - yUAV_E);
+c = zUAV_S - mx*yUAV_S;
+
+% 3D Plot labels
+xlim([0 2050])
+ylim([0 2050])
+zlim([0 320])
+xlabel('Width (m)');
+ylabel('Length (m)');
+zlabel('Height (m)');
+grid on; hold on; drawnow
+
+%% Information for path loss model
+
+eta = 2.5;              % Path Loss Component
+b_0dB = -50;            % Reference Channel Gain in dB
+b_0 = db2pow(b_0dB);    % Reference Channel Gain in linear scale
+k = 0.01;               % Additional attenuation for NLoS
+
+%% Infromation for Rician Fading Model
+
+% Rician factor
+K_min = 4;      % K_min value in dB
+K_max = 12;     % K_max value in dB
+
+A1 = db2pow(K_min);
+A2 = (2/pi)*log((db2pow(K_max))/A1);
+
+%N = 10^5;
+g = sqrt(1/2)*(randn(1,1)+1i*randn(1,1));
+
+
+%% Simulation
+
+for j = enLength:-100:0
+    for i=1:noUAV
+        zUAV(i) = mx*j + c;
+        yUAV(i) = j;
+        plot3(xUAV(i),  yUAV(i),  zUAV(i), 'r+','linewidth', 0.5);hold on;
+
+        % LoS distance between UAVs and BSs
+        for bm=1:noBS
+            groundDisUAV_BS(i,bm) = sqrt((xUAV(i)-xBS(bm))^2 + (yUAV(i)-yBS(bm))^2);
+            DisUAV_BS(i,bm) = sqrt(groundDisUAV_BS(i,bm)^2 + (zBS(bm)-zUAV(i))^2);
+            
+            % Elavation Angle in radiant between UAVs and Users
+            angleUAV_BS(i,bm) = atan(abs(zBS(bm)-zUAV(i))/groundDisUAV_BS(i,bm))*(180/pi);
+            
+            PLoS_BS(index,i,bm) = 1/(1+(10*exp(-0.6*(angleUAV_BS(i,bm)-10))));
+            
+            pow_LoS_BS = b_0*(DisUAV_BS(i,bm)^(-eta));
+            pow_NLoS_BS = k*b_0*(DisUAV_BS(i,bm)^(-eta));
+            Ch_pow_LoS_BS(index,i,bm) = pow2db(pow_LoS_BS); 
+            Ch_pow_NLoS_BS(index,i,bm) = pow2db(pow_NLoS_BS);
+            
+            % Expected Path Loss Channel gain
+            E_bd_BS = PLoS_BS(index,i,bm)*pow_LoS_BS + (1 - PLoS_BS(index,i,bm))*pow_NLoS_BS;
+            E_bd_dB_BS(index,i,bm) = pow2db(E_bd_BS); % in dB
+            
+            % Angle depend rician factor
+            K_UAV_BS(i,bm) = A1*exp(A2*angleUAV_BS(i,bm)*(pi/180));
+            
+            g_UAV_BS(i,bm) = sqrt(K_UAV_BS(i,bm)/(1+K_UAV_BS(i,bm)))*g + sqrt(1/(1+K_UAV_BS(i,bm)))*g;
+            
+            h_UAV_BS(index,i,bm) = sqrt(pow_LoS_BS)*g_UAV_BS(i,bm);
+            
+            h_UAV_BS_dB(index,i,bm) = pow2db(abs(h_UAV_BS(index,i,bm))^2);
+            
+        end
+        
+        % LoS distance between UAVs and Users
+        for m=1:noUsers
+            groundDisUAV_User(i,m) = sqrt((xUAV(i)-xUser(m))^2 + (yUAV(i)-yUser(m))^2);
+            DisUAV_User(i,m) = sqrt(groundDisUAV_User(i,m)^2 + zUAV(i)^2);
+            
+            % Elavation Angle in radiant between UAVs and Users
+            angleUAV_User(i,m) = atan(zUAV(i)/groundDisUAV_User(i,m))*(180/pi);
+            
+            
+            PLoS(index,i,m) = 1/(1+(10*exp(-0.6*(angleUAV_User(i,m)-10))));
+            
+            pow_LoS = b_0*(DisUAV_User(i,m)^(-eta));
+            pow_NLoS = k*b_0*(DisUAV_User(i,m)^(-eta));
+            Ch_pow_LoS(index,i,m) = pow2db(pow_LoS); 
+            Ch_pow_NLoS(index,i,m) = pow2db(pow_NLoS);
+            
+            % Expected Path Loss Channel gain
+            E_bd = PLoS(index,i,m)*pow_LoS + (1 - PLoS(index,i,m))*pow_NLoS;
+            E_bd_dB(index,i,m) = pow2db(E_bd); % in dB
+            
+            
+            % Angle depend rician factor
+            K_UAV_User(i,m) = A1*exp(A2*angleUAV_User(i,m)*(pi/180));
+            
+            g_UAV_User(i,m) = sqrt(K_UAV_User(i,m)/(1+K_UAV_User(i,m)))*g + sqrt(1/(1+K_UAV_User(i,m)))*g;
+            
+            h_UAV_Users(index,i,m) = sqrt(pow_LoS)*g_UAV_User(i,m);
+            
+            h_UAV_Users_dB(index,i,m) = pow2db(abs(h_UAV_Users(index,i,m))^2);
+           
+        end
+        
+        % Power coefficeints calculation for users
+        [pow_coef_array_ch(index,:), pow_coef_array_fr(index,:)] = findPowCoeff(abs(h_UAV_Users(index,i,:)),noUsers);
+        
+        % Achievable Rate Calculations for Users
+        [achievableRate_ch(index,:), achievableRate_fr(index,:)] = findAchievableRate(h_UAV_Users(index,i,:),pow_coef_array_ch(index,:),pow_coef_array_fr(index,:),noUsers);
+        
+        % Achievable Rate Calculations for BSs
+        achievableRate_BS(index,:) = findAchievableRate_BS(h_UAV_BS(index,i,:),noBS);
+        
+        % Achievable Rate Calculations for Users in SWIPT model
+        [achievableRate_ch_SWIPT(index,:), achievableRate_fr_SWIPT(index,:)] = findAchievableRate_SWIPT(h_UAV_Users(index,i,:),h_UAV_BS(index,i,:),pow_coef_array_ch(index,:),pow_coef_array_fr(index,:),noUsers);
+        
+        minRate = min(achievableRate_ch(index,:))
+        if index>1 && index<21
+            updatedPosition = positionUpdate_PSO(xUAV(i),yUAV(i),zUAV(i),minRate,noUsers,xUser,yUser,g);
+            up_xUAV(index,i) = xUAV(i) + updatedPosition.position(1);
+            up_zUAV(index,i) = zUAV(i) + updatedPosition.position(2);
+            up_yUAV(index,i) = yUAV(i);
+        else
+            up_xUAV(index,i) = xUAV(i);
+            up_yUAV(index,i) = yUAV(i);
+            up_zUAV(index,i) = zUAV(i);
+        end
+        plot3(up_xUAV(index,i),  up_yUAV(index,i),  up_zUAV(index,i), 'gh','linewidth', 0.5);hold on;
+        
+        if index>1 
+            plot3([up_xUAV(index-1,i) up_xUAV(index,i)], [up_yUAV(index-1,i) up_yUAV(index,i)],[up_zUAV(index-1,i) up_zUAV(index,i)], 'g--','linewidth', 0.5); drawnow
+        end
+       % updatedPosition.minRate
+        % LoS distance between UAVs and BSs
+        for bm=1:noBS
+            groundDisUAV_BS_Up(i,bm) = sqrt((up_xUAV(index,i)-xBS(bm))^2 + (up_yUAV(index,i)-yBS(bm))^2);
+            DisUAV_BS_Up(i,bm) = sqrt(groundDisUAV_BS_Up(i,bm)^2 + (zBS(bm)-up_zUAV(index,i))^2);
+            
+            % Elavation Angle in radiant between UAVs and Users
+            angleUAV_BS_Up(i,bm) = atan(abs(zBS(bm)-up_zUAV(index,i))/groundDisUAV_BS_Up(i,bm))*(180/pi);
+            
+            PLoS_BS_Up(index,i,bm) = 1/(1+(10*exp(-0.6*(angleUAV_BS_Up(i,bm)-10))));
+            
+            pow_LoS_BS_Up = b_0*(DisUAV_BS_Up(i,bm)^(-eta));
+            pow_NLoS_BS_Up = k*b_0*(DisUAV_BS_Up(i,bm)^(-eta));
+            Ch_pow_LoS_BS_Up(index,i,bm) = pow2db(pow_LoS_BS_Up); 
+            Ch_pow_NLoS_BS_Up(index,i,bm) = pow2db(pow_NLoS_BS_Up);
+            
+            % Expected Path Loss Channel gain
+            E_bd_BS_Up = PLoS_BS_Up(index,i,bm)*pow_LoS_BS_Up + (1 - PLoS_BS_Up(index,i,bm))*pow_NLoS_BS_Up;
+            E_bd_dB_BS_Up(index,i,bm) = pow2db(E_bd_BS_Up); % in dB
+            
+            % Angle depend rician factor
+            K_UAV_BS_Up(i,bm) = A1*exp(A2*angleUAV_BS_Up(i,bm)*(pi/180));
+            
+            g_UAV_BS_Up(i,bm) = sqrt(K_UAV_BS_Up(i,bm)/(1+K_UAV_BS_Up(i,bm)))*g + sqrt(1/(1+K_UAV_BS_Up(i,bm)))*g;
+            
+            h_UAV_BS_Up(index,i,bm) = sqrt(pow_LoS_BS_Up)*g_UAV_BS_Up(i,bm);
+            
+            h_UAV_BS_dB_Up(index,i,bm) = pow2db(abs(h_UAV_BS_Up(index,i,bm))^2);
+            
+        end
+        
+        % LoS distance between UAVs and Users
+        for m=1:noUsers
+            groundDisUAV_User_Up(i,m) = sqrt((up_xUAV(index,i)-xUser(m))^2 + (up_yUAV(index,i)-yUser(m))^2);
+            DisUAV_User_Up(i,m) = sqrt(groundDisUAV_User_Up(i,m)^2 + up_zUAV(index,i)^2);
+            
+            % Elavation Angle in radiant between UAVs and Users
+            angleUAV_User_Up(i,m) = atan(up_zUAV(index,i)/groundDisUAV_User_Up(i,m))*(180/pi);
+            
+            
+            PLoS_Up(index,i,m) = 1/(1+(10*exp(-0.6*(angleUAV_User_Up(i,m)-10))));
+            
+            pow_LoS_Up = b_0*(DisUAV_User_Up(i,m)^(-eta));
+            pow_NLoS_Up = k*b_0*(DisUAV_User_Up(i,m)^(-eta));
+            Ch_pow_LoS_Up(index,i,m) = pow2db(pow_LoS_Up); 
+            Ch_pow_NLoS_Up(index,i,m) = pow2db(pow_NLoS_Up);
+            
+            % Expected Path Loss Channel gain
+            E_bd_Up = PLoS_Up(index,i,m)*pow_LoS_Up + (1 - PLoS_Up(index,i,m))*pow_NLoS_Up;
+            E_bd_dB_Up(index,i,m) = pow2db(E_bd_Up); % in dB
+            
+            
+            % Angle depend rician factor
+            K_UAV_User_Up(i,m) = A1*exp(A2*angleUAV_User_Up(i,m)*(pi/180));
+            
+            g_UAV_User_Up(i,m) = sqrt(K_UAV_User_Up(i,m)/(1+K_UAV_User_Up(i,m)))*g + sqrt(1/(1+K_UAV_User_Up(i,m)))*g;
+            
+            h_UAV_Users_Up(index,i,m) = sqrt(pow_LoS_Up)*g_UAV_User_Up(i,m);
+            
+            h_UAV_Users_dB_Up(index,i,m) = pow2db(abs(h_UAV_Users_Up(index,i,m))^2);
+           
+        end
+%         %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%         minRate_SWIPT = min(achievableRate_ch_SWIPT(index,:))
+%         if index>1 && index<21
+%             updatedPosition_SWIPT = positionUpdate_PSO_SWIPT(xUAV(i),yUAV(i),zUAV(i),xBS,yBS,zBS,minRate,noUsers,xUser,yUser,g,noBS);
+%             up_xUAV_SWIPT(index,i) = xUAV(i) + updatedPosition_SWIPT.position(1);
+%             up_zUAV_SWIPT(index,i) = zUAV(i) + updatedPosition_SWIPT.position(2);
+%             up_yUAV_SWIPT(index,i) = yUAV(i);
+%         else
+%             up_xUAV_SWIPT(index,i) = xUAV(i);
+%             up_yUAV_SWIPT(index,i) = yUAV(i);
+%             up_zUAV_SWIPT(index,i) = zUAV(i);
+%         end
+%         plot3(up_xUAV_SWIPT(index,i),  up_yUAV_SWIPT(index,i),  up_zUAV_SWIPT(index,i), 'gp','linewidth', 0.5);hold on;
+%         
+%         if index>1 
+%             plot3([up_xUAV_SWIPT(index-1,i) up_xUAV_SWIPT(index,i)], [up_yUAV_SWIPT(index-1,i) up_yUAV_SWIPT(index,i)],[up_zUAV_SWIPT(index-1,i) up_zUAV_SWIPT(index,i)], 'm--','linewidth', 0.5); drawnow
+%         end
+%        % updatedPosition.minRate
+%         % LoS distance between UAVs and BSs
+%         for bm=1:noBS
+%             groundDisUAV_BS_Up_SWIPT(i,bm) = sqrt((up_xUAV_SWIPT(index,i)-xBS(bm))^2 + (up_yUAV_SWIPT(index,i)-yBS(bm))^2);
+%             DisUAV_BS_Up_SWIPT(i,bm) = sqrt(groundDisUAV_BS_Up_SWIPT(i,bm)^2 + (zBS(bm)-up_zUAV_SWIPT(index,i))^2);
+%             
+%             % Elavation Angle in radiant between UAVs and Users
+%             angleUAV_BS_Up_SWIPT(i,bm) = atan(abs(zBS(bm)-up_zUAV_SWIPT(index,i))/groundDisUAV_BS_Up_SWIPT(i,bm))*(180/pi);
+%             
+%             PLoS_BS_Up_SWIPT(index,i,bm) = 1/(1+(10*exp(-0.6*(angleUAV_BS_Up_SWIPT(i,bm)-10))));
+%             
+%             pow_LoS_BS_Up_SWIPT = b_0*(DisUAV_BS_Up_SWIPT(i,bm)^(-eta));
+%             pow_NLoS_BS_Up_SWIPT = k*b_0*(DisUAV_BS_Up_SWIPT(i,bm)^(-eta));
+%             Ch_pow_LoS_BS_Up_SWIPT(index,i,bm) = pow2db(pow_LoS_BS_Up_SWIPT); 
+%             Ch_pow_NLoS_BS_Up_SWIPT(index,i,bm) = pow2db(pow_NLoS_BS_Up_SWIPT);
+%             
+%             % Expected Path Loss Channel gain
+%             E_bd_BS_Up_SWIPT = PLoS_BS_Up_SWIPT(index,i,bm)*pow_LoS_BS_Up_SWIPT + (1 - PLoS_BS_Up_SWIPT(index,i,bm))*pow_NLoS_BS_Up_SWIPT;
+%             E_bd_dB_BS_Up_SWIPT(index,i,bm) = pow2db(E_bd_BS_Up_SWIPT); % in dB
+%             
+%             % Angle depend rician factor
+%             K_UAV_BS_Up_SWIPT(i,bm) = A1*exp(A2*angleUAV_BS_Up_SWIPT(i,bm)*(pi/180));
+%             
+%             g_UAV_BS_Up_SWIPT(i,bm) = sqrt(K_UAV_BS_Up_SWIPT(i,bm)/(1+K_UAV_BS_Up_SWIPT(i,bm)))*g + sqrt(1/(1+K_UAV_BS_Up_SWIPT(i,bm)))*g;
+%             
+%             h_UAV_BS_Up_SWIPT(index,i,bm) = sqrt(pow_LoS_BS_Up_SWIPT)*g_UAV_BS_Up_SWIPT(i,bm);
+%             
+%             h_UAV_BS_dB_Up_SWIPT(index,i,bm) = pow2db(abs(h_UAV_BS_Up_SWIPT(index,i,bm))^2);
+%             
+%         end
+%         
+%         % LoS distance between UAVs and Users
+%         for m=1:noUsers
+%             groundDisUAV_User_Up_SWIPT(i,m) = sqrt((up_xUAV_SWIPT(index,i)-xUser(m))^2 + (up_yUAV_SWIPT(index,i)-yUser(m))^2);
+%             DisUAV_User_Up_SWIPT(i,m) = sqrt(groundDisUAV_User_Up_SWIPT(i,m)^2 + up_zUAV_SWIPT(index,i)^2);
+%             
+%             % Elavation Angle in radiant between UAVs and Users
+%             angleUAV_User_Up_SWIPT(i,m) = atan(up_zUAV_SWIPT(index,i)/groundDisUAV_User_Up_SWIPT(i,m))*(180/pi);
+%             
+%             
+%             PLoS_Up_SWIPT(index,i,m) = 1/(1+(10*exp(-0.6*(angleUAV_User_Up_SWIPT(i,m)-10))));
+%             
+%             pow_LoS_Up_SWIPT = b_0*(DisUAV_User_Up_SWIPT(i,m)^(-eta));
+%             pow_NLoS_Up_SWIPT = k*b_0*(DisUAV_User_Up_SWIPT(i,m)^(-eta));
+%             Ch_pow_LoS_Up_SWIPT(index,i,m) = pow2db(pow_LoS_Up_SWIPT); 
+%             Ch_pow_NLoS_Up_SWIPT(index,i,m) = pow2db(pow_NLoS_Up_SWIPT);
+%             
+%             % Expected Path Loss Channel gain
+%             E_bd_Up_SWIPT = PLoS_Up_SWIPT(index,i,m)*pow_LoS_Up_SWIPT + (1 - PLoS_Up_SWIPT(index,i,m))*pow_NLoS_Up_SWIPT;
+%             E_bd_dB_Up_SWIPT(index,i,m) = pow2db(E_bd_Up_SWIPT); % in dB
+%             
+%             
+%             % Angle depend rician factor
+%             K_UAV_User_Up_SWIPT(i,m) = A1*exp(A2*angleUAV_User_Up_SWIPT(i,m)*(pi/180));
+%             
+%             g_UAV_User_Up_SWIPT(i,m) = sqrt(K_UAV_User_Up_SWIPT(i,m)/(1+K_UAV_User_Up_SWIPT(i,m)))*g + sqrt(1/(1+K_UAV_User_Up_SWIPT(i,m)))*g;
+%             
+%             h_UAV_Users_Up_SWIPT(index,i,m) = sqrt(pow_LoS_Up_SWIPT)*g_UAV_User_Up_SWIPT(i,m);
+%             
+%             h_UAV_Users_dB_Up_SWIPT(index,i,m) = pow2db(abs(h_UAV_Users_Up_SWIPT(index,i,m))^2);
+%            
+%         end
+% 
+%         
+%         
+%         
+%         %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+        % Power coefficeints calculation for users
+        [pow_coef_array_ch_Up(index,:), pow_coef_array_fr_Up(index,:)] = findPowCoeff(abs(h_UAV_Users_Up(index,i,:)),noUsers);
+        
+        % Achievable Rate Calculations for Users
+        [achievableRate_ch_Up(index,:), achievableRate_fr_Up(index,:)] = findAchievableRate(h_UAV_Users_Up(index,i,:),pow_coef_array_ch_Up(index,:),pow_coef_array_fr_Up(index,:),noUsers);
+        
+        % Achievable Rate Calculations for BSs
+        achievableRate_BS_Up(index,:) = findAchievableRate_BS(h_UAV_BS_Up(index,i,:),noBS);
+        
+        % Achievable Rate Calculations for Users in SWIPT model
+        [achievableRate_ch_SWIPT_Up(index,:), achievableRate_fr_SWIPT_Up(index,:)] = findAchievableRate_SWIPT(h_UAV_Users_Up(index,i,:),h_UAV_BS_Up(index,i,:),pow_coef_array_ch_Up(index,:),pow_coef_array_fr_Up(index,:),noUsers);
+        
+        minAchRate(index) = min(achievableRate_ch(index,:));
+        minAchRate_Up(index) = min(achievableRate_ch_Up(index,:));
+        
+        minAchRate_SWIPT(index) = min(achievableRate_ch_SWIPT(index,:));
+        minAchRate_Up_SWIPT(index) = min(achievableRate_ch_SWIPT_Up(index,:));
+        
+        
+%     
+%         
+%         %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%         
+%         % Power coefficeints calculation for users
+%         [pow_coef_array_ch_Up_SWIPT(index,:), pow_coef_array_fr_Up_SWIPT(index,:)] = findPowCoeff(abs(h_UAV_Users_Up_SWIPT(index,i,:)),noUsers);
+%         
+%         % Achievable Rate Calculations for Users
+%         %[achievableRate_ch_Up_SWIPT(index,:), achievableRate_fr_Up_SWIPT(index,:)] = findAchievableRate(h_UAV_Users_Up_SWIPT(index,i,:),pow_coef_array_ch_Up_SWIPT(index,:),pow_coef_array_fr_Up_SWIPT(index,:),noUsers);
+%         
+%         % Achievable Rate Calculations for BSs
+%         %achievableRate_BS_Up_SWIPT(index,:) = findAchievableRate_BS(h_UAV_BS_Up_SWIPT(index,i,:),noBS);
+%         
+%         % Achievable Rate Calculations for Users in SWIPT model
+%         [achievableRate_ch_SWIPT_Up(index,:), achievableRate_fr_SWIPT_Up(index,:)] = findAchievableRate_SWIPT(h_UAV_Users_Up_SWIPT(index,i,:),h_UAV_BS_Up_SWIPT(index,i,:),pow_coef_array_ch_Up_SWIPT(index,:),pow_coef_array_fr_Up_SWIPT(index,:),noUsers);
+%         
+%         %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%         
+%         minAchRate_SWIPT(index) = min(achievableRate_ch_SWIPT(index,:));
+%         minAchRate_Up_SWIPT(index) = min(achievableRate_ch_SWIPT_Up(index,:));
+%         
+        
+    end
+    index = index +1;
+end
+
 
 
